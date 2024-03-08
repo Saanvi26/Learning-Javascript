27a7970c0897001f05e63aa64d0f61c2e5decf32
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 23:08:27 +0530

    comparisions in js

---
+++ basics/04.js
@@ -0,0 +1,12 @@
+console.log(null==0)
+console.log(null===0)
+console.log(null>=0)
+console.log(null<=0)
+console.log(null>0)
+console.log(null<0)
+console.log(null!=0)
+console.log(null==undefined)
+console.log(null===undefined)
+//=== strict check : checks the datatype also
+console.log("2"==2);
+console.log("2"===2);
