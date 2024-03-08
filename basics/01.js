eec6c4dc635d9dcc24707367d1ab4c1752eaba75
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 22:45:06 +0530

    datatype

---
+++ basics/01.js
@@ -0,0 +1,7 @@
+"use strict"; //use strict mode to avoid redefining of variables
+//alert(2+2); //not allowed in node.js
+console.log(typeof 2); //number
+console.log(typeof "2"); //string
+console.log(typeof null);
+// ** null is an object **
+console.log(typeof undefined);
