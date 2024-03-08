47c7eb748556b680a8964051fc64adb242ec07ff
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-09 00:47:02 +0530

    Dates in js

---
+++ basics/09.js
@@ -0,0 +1,15 @@
+//Date Object in js beginning from 1 Jan 1970
+// Date object is in millisec
+console.log(new Date());
+console.log(new Date().getTime());
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth());
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(new Date().getSeconds());
+console.log(new Date().toString());
+console.log(new Date().toDateString());
+console.log(new Date().toLocaleString());
+console.log(new Date().toJSON());
