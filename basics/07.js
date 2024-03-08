4d73f2770282d760812ea37d2319356aba249240
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 23:51:01 +0530

    String functions

---
+++ basics/07.js
@@ -0,0 +1,21 @@
+const name="Saanvi";
+console.log(`Hello my name is ${name}`);
+
+const otherName=new String("Pranshu");
+console.log(otherName);
+console.log(otherName[0]);
+console.log(typeof(name));
+console.log(typeof(otherName));
+
+console.log(name.length);
+
+const name2=name.slice(0,3);
+console.log(name2);
+const name3=name.slice(-6,6,-1);
+console.log(name3);
+
+
+const url="https://www.linkedin.com/saanvi26@lakhanpal";
+console.log(url.replace("26@","-"));
+console.log(url.split("/"));
+
