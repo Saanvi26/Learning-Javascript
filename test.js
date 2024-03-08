b3414cc73495842f03b4ee0b72d3498c60bf08ff
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 22:45:24 +0530

    js basic

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
 console.log("Hello");
 const accountId=144553; //cannot be changed
-let accountEmail="saanvi@gmail.com"; //can be changed :- new code
-var accountPassword="saanvi123"; //can be changed :-old
+let accountEmail="saanvi@gmail.com"; //can be changed :- new method
+var accountPassword="saanvi123"; //can be changed :-old method
 accountCity="Dehradun";
+let accountState;
 // accountId=4  Error
 // console.log(accountId) 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
 /*
 Prefer not to use var because of its global scope 
 */
+
+var a=1;
+var a=5;
+console.log(a); //redefining with var is the issue and not a good practice
+let b=1;
+// let b=5; //Error
+console.log(b);
