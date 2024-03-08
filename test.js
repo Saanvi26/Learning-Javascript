0686295cc9d8b660ba9f6690b9813a532c79dd92
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 22:04:12 +0530

    let const var

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 console.log("Hello");
+const accountId=144553; //cannot be changed
+let accountEmail="saanvi@gmail.com"; //can be changed :- new code
+var accountPassword="saanvi123"; //can be changed :-old
+accountCity="Dehradun";
+// accountId=4  Error
+// console.log(accountId) 
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+Prefer not to use var because of its global scope 
+*/
