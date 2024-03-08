a4d5b8ba7a834a14e3b1869c05ebdd27b1b6674d
Saanvi26 <129958210+Saanvi26@users.noreply.github.com>
2024-03-08 23:03:04 +0530

    Tricky operations in js

---
+++ basics/03.js
@@ -0,0 +1,19 @@
+//operations
+let value1=10;
+let value2=20;
+let negValue=-value1;
+console.log(negValue); //-10
+console.log(2**3); // 2^3=8
+let fname="Saanvi";
+let lname=" Lakhanpal";
+console.log(fname+lname); //Saanvi Lakhanpal
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2);
+console.log("1"+2+3);
+console.log(1+2+"3");
+console.log(+"");
+console.log(+true);
+
+
